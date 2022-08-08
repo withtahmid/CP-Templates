e140e4b4e0e03ab3d8b493a1d71f6f8222cb028f
tahmid <tahmid@tahmid.tahmid>
2022-08-09 02:18:18 +0600

    09/08/2022

--- AutoMate.cpp
+++ AutoMate.cpp
@@ -2,6 +2,7 @@
  * 
  * Author: withtahmid
  * Created: 16-07-22 23:59
+ * Updated: 09-08-22 02:15
  * 
  * */
 #include<bits/stdc++.h>
@@ -10,8 +11,9 @@
 #include<unistd.h> 
 #include <fstream>
 using namespace std;
-string mainPath = "";
 string dateNtime = " * Created: ";
+string path = "Codes/";
+int numOfFile;
 void generate_date()
 {
 	time_t rawtime;
@@ -24,173 +26,118 @@ void generate_date()
   	dateNtime += str;
 }
 
-
-int main()
+void generate_path()
 {
-	generate_date();
 	char c;
-	cin>>c; 
+	char type;
+	string folderName;
+	cin >> c >> type >> folderName >> numOfFile;
 	if(c == 'C' || c == 'c')
-	{
-		int div;
-		cin>>div;
-		cin.ignore();
-		string folderName;
-		cin>>folderName;
-		int numOfFile;
-		cin>>numOfFile;
-		
-		string path = "Codes/CodeForces/div";
-		path += div + '0';
+	{		
+		path += "CodeForces/div";
+		path += type;
 		path += "/";
 		path += folderName;
 		path += '/';
-		mainPath = path;
-		if(!mkdir(path.c_str(), 0777))
-		{
-			for(int i = 0; i < numOfFile; i++)
-			{
-				string fileName = path;
-			    fileName += 'A' + i;
-			    fileName += ".cpp";
-				string line;
-				    ifstream ini_file{
-				        "Template.cpp"
-				    };
-				    
-				    ofstream out_file{ fileName};
-				    if (ini_file && out_file) {
-				  
-				        while (getline(ini_file, line)) {
-				        	if(line == " * Created: dd-mm-yy hh:mm")
-				        		line = dateNtime;
-
-				            out_file << line << "\n";
-				        }
-				    }
-				    else {
-				       cout<< "Cannot create file: "<<i<<endl;
-				    }
-				    ini_file.close();
-				    out_file.close();
-				    string command ="subl "; 
-				    command += fileName;
-				    system(command.c_str());
-			}
-		}
-		else
-		{
-			cout<<"Some error occoured\n";
-		}
 	}
 	else if(c == 'A' || c == 'a')
 	{
-		cin.ignore();
-		char type;
-		cin>>type;
-		string folderName;
-		cin.ignore();
-		cin>>folderName;
-		int numOfFile;
-		cin>>numOfFile;
 		
-		string path = "Codes/Atcoder/";
+		path += "Atcoder/";
 		type == 'b' || type == 'B' ? path +=  "Beginner" : path +=  "Regular" ;
 		path += "/";
 		path += folderName;
 		path += '/';
-		mainPath = path;
-		if(!mkdir(path.c_str(), 0777))
-		{
-			for(int i = 0; i < numOfFile; i++)
-			{
-				string fileName = path;
-			    fileName += 'A' + i;
-			    fileName += ".cpp";
-				string line;
-				    ifstream ini_file{
-				        "Template.cpp"
-				    };
-				    
-				    ofstream out_file{ fileName};
-				    if (ini_file && out_file) {
-				  
-				        while (getline(ini_file, line)) {
-				        	if(line == " * Created: dd-mm-yy hh:mm")
-				        		line = dateNtime;
-				            out_file << line << "\n";
-				        }
-				        //cout << " File: "<<i<< ".cpp copy Finished \n";
-				    }
-				    else {
-				       cout<< "Cannot create file: "<<i<<endl;
-				    }
-				    ini_file.close();
-				    out_file.close();
-				     string command ="subl "; 
-				    command += fileName;
-				    system(command.c_str());
-			}
-		}
-
-		else
-		{
-			cout<<"Some error occoured\n";
-		}
-
 	}
-	else if(c == 'x' || c == 'X')
+	else
 	{
-		string path;
-		cin>>path;
-		int numOfFile;
-		cin>>numOfFile;
-		path += '/';
-		mainPath = path;
-		if(!mkdir(path.c_str(), 0777))
-		{
-			for(int i = 0; i < numOfFile; i++)
-			{
-				string fileName = path;
-			    fileName += 'A' + i;
-			    fileName += ".cpp";
-				string line;
-				    ifstream ini_file{
-				        "Template.cpp"
-				    };
-				    
-				    ofstream out_file{ fileName};
-				    if (ini_file && out_file) {
-				  
-				        while (getline(ini_file, line)) {
-				        	if(line == " * Created: dd-mm-yy hh:mm")
-				        		line = dateNtime;
-				            out_file << line << "\n";
-				        }
-				        //cout << " File: "<<i<< ".cpp copy Finished \n";
-				    }
-				    else {
-				       cout<< "Cannot create file: "<<i<<endl;
-				    }
-				    ini_file.close();
-				    out_file.close();
-				     string command ="subl "; 
-				    command += fileName;
-				    system(command.c_str());
-			}
-		}
+		path = "no_path";
+	}
+}
 
-		else
+
+
+bool create_files()
+{	
+	bool yes = true;
+
+	if(!mkdir(path.c_str(), 0777))
+	{
+		for(int i = 0; i < numOfFile; i++)
 		{
-			cout<<"Some error occoured\n";
+			string fileName = path;
+		    fileName += 'A' + i;
+		    fileName += ".cpp";
+			string line;
+		    ifstream ini_file{
+		        "Template.cpp"
+		    };
+		    
+		    ofstream out_file{ fileName};
+		    if (ini_file && out_file) {
+		  
+		        while (getline(ini_file, line)) {
+		        	if(line == " * Created: dd-mm-yy hh:mm")
+		        		line = dateNtime;
+		            out_file << line << "\n";
+		        }
+		    }
+		    else {
+		       cout<< "Cannot create file: "<<i<<endl;
+		    	return false;
+		    }
+		    ini_file.close();
+		    out_file.close();
 		}
 	}
 	else
 	{
-		string str; getline(cin,str);
-		cout<<"ERROR!!!\n";
+		cout<<"Some error occoured\n";
+		return false;
 	}
-	cout << endl << "cd "<<mainPath<<endl;
-	string command = "cd ";
-	command += mainPath;
+	return true;
+}
+void create_io_files()
+{
+	ofstream input("input.txt");
+	input.close();
+	ofstream output("output.txt");
+	output.close();
+}
+void open_files_in_sublime_text()
+{
+	string command ="subl "; 
+	command += path;
+	command += "A.cpp";
+	system(command.c_str());
+	command ="subl "; 
+	command += path;
+	command += "input.txt";
+	system(command.c_str());
+	command ="subl "; 
+	command += path;
+	command += "output.txt";
+	system(command.c_str());
+}
+int main()
+{
+	generate_date();
+	generate_path();
+	if(path != "no-path" && numOfFile > 0)
+	{
+		if(create_files())
+		{
+			//create_io_files();
+			open_files_in_sublime_text();
+			cout << endl << "cd "<<path<<endl;
+		}
+		else
+			cout << "Couldn't create file\n";
+		
+		
+	}
+	else
+	
+		cout << "Invalid Path or File number\n";
+
 }
